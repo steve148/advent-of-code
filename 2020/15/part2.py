592731a1e9646736c7cc8a1a46c09b84012a448f
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2020-12-15 01:26:28 -0500

    feat: day 15

---
+++ 2020/15/part2.py
@@ -0,0 +1,53 @@
+from collections.abc import Iterable
+
+
+def get_input():
+    with open("input") as f:
+        lines = f.readlines()
+        return [int(x) for x in lines[0].split(",")]
+
+
+def update_state(value: int, index: int, state: dict) -> dict:
+    if value not in state.keys():
+        state[value] = [index]
+    else:
+        state[value].append(index)
+
+    return state
+
+
+def infinite_answers(starting_list: list[int]) -> Iterable[int]:
+    state = {}
+    current_index = 1
+    last_element = -1
+
+    while True:
+        if current_index < len(starting_list) + 1:
+            next_element = starting_list[current_index - 1]
+            if last_element != -1:
+                state = update_state(last_element, current_index - 1, state)
+            yield next_element
+        else:
+
+            if last_element not in state.keys():
+                next_element = 0
+                state = update_state(last_element, current_index - 1, state)
+                yield next_element
+            else:
+                next_element = current_index - 1 - state[last_element][-1]
+                state = update_state(last_element, current_index - 1, state)
+                yield next_element
+
+        last_element = next_element
+        current_index += 1
+
+
+def main() -> int:
+    starting_numbers = get_input()
+
+    for i, num in enumerate(infinite_answers(starting_list=starting_numbers)):
+        if i == 30000000 - 1:
+            return num
+
+
+print(main())
